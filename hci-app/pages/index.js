66e50777afd0d675402c818c825cb82c6663149a
ivan <icalet00@fesb.hr>
2022-12-13 00:33:25 +0100

    init

---
+++ hci-app/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Footer from '../components/Footer.js';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Restaurant</title>
+        <meta name="description" content="Food ordering app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      homepage
+    </div>
+  )
+}
