631a8072190737fbb6eb453b74685b8d94f78f8f
Stipe-Kurir <skurir00@fesb.hr>
2022-12-23 21:23:53 +0100

    stipe 3. commit

--- hci-app/components/Footer.js
+++ hci-app/components/Footer.js
@@ -1,7 +1,54 @@
+import styles from "../styles/Footer.module.css"
+import Image from "next/image";
+
+
 const Footer =()=>{
     return (
-        <div>
-                footer
+        <div className={styles.container}>
+            <div className={styles.item}>
+                <Image src="/img/bg.png" layout="fill" alt=""/>
+            </div>
+            <div className={styles.item}>
+            <div className={styles.card}>
+                <h2 className={styles.motto}>
+                    OH YES,WE DID.WELL BAKED SLICE OF PIZZA.
+                </h2>
+            </div>
+            <div className={styles.card}>
+        <h1 className={styles.title}>FIND OUR RESTOURANTS</h1>
+        <p className={styles.text}>
+          Ul. Ruđera Boškovića 32  
+        <br />21000, Split
+        <br />021 123 777
+        </p>
+        <p className={styles.text}>
+          Ul. Ruđera Boškovića 32  
+        <br />21000, Split
+        <br />021 123 777
+        </p>
+        <p className={styles.text}>
+          Ul. Ruđera Boškovića 32 
+        <br />21000, Split
+        <br />021 123 777
+        </p>
+            </div>
+
+            <div className={styles.card}>
+                <h1 className={styles.title}> WORKING HOURS</h1>
+                <p className={styles.text}>
+                    MONDAY UNTIL FRIDAY
+                    <br />9:00 - 22:00
+                </p>
+                <p className={styles.text}>
+                    SATURDAY - SUNDAY
+                    <br />12:00 - 24:00
+                </p>
+
+
+            </div>
+
+            </div>
+            
         </div>
     )
 
