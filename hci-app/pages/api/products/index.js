e0c40a9d178738c1535ffc29d0d0475ce8addcc4
IvanCaleta <icalet00@fesb.hr>
2023-01-25 16:48:02 +0100

    db connection

--- hci-app/pages/api/products/index.js
+++ hci-app/pages/api/products/index.js
@@ -1,16 +1,34 @@
-export default function handler(req, res) {
-   const method=req.method;
-   
-   const products=require('../../../models/Product.json')
-  
-   if(method==='GET'){
-        try {
-            res.status(200).send(products)
-        } catch (error) {
-            console.log('aaa')
-            res.status(500).json(error)
-        }
+import connectDB from "../../../util/mongo"
+import Prod from "../../../models/Product"
+
+/**
+ * 
+ * @param {import ("next").NextApiRequest}req
+ * @param {import("next").NextApiResponse}res
+ * 
+ */
+export default async function handler(req, res) {
+  const { method} = req;
+
+  console.log("Connecting DB...")
+  connectDB()
+  console.log("Connected")
+
+  if (method === "GET") {
+    try {
+      const products = await Prod.find();
+      res.status(200).json(products);
+    } catch (err) {
+      res.status(500).json(err);
     }
-   
   }
-  
+
+  if (method === "POST") {
+    try {
+      const product = await Prod.create(req.body);
+      res.status(201).json(product);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  }
+}
--- hci-app/util/mongo.js
+++ hci-app/util/mongo.js
@@ -1,42 +1,5 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose"
 
-const MONGODB_URL = "mongodb://localhost/restaurant"
+const connectDB=async()=> mongoose.connect("mongodb://localhost:27017/restaurant")
 
-if (!MONGODB_URL) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  )
-}
-
-let cached = global.mongoose
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null }
-}
-
-async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    }
-
-    cached.promise = mongoose.connect(MONGODB_URL, opts).then((mongoose) => {
-      return mongoose
-    })
-  }
-
-  try {
-    cached.conn = await cached.promise
-  } catch (e) {
-    cached.promise = null
-    throw e
-  }
-
-  return cached.conn
-}
-
-export default dbConnect
+export default connectDB
